71e559ca673822ec27451f7ed98f9d9f051a0409
RyoutaKaneko <k021g1476@g.neec.ac.jp>
2022-04-25 16:20:52 +0900

    カメラ追従

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -20,11 +20,20 @@ void GameScene::Initialize() {
 	debugText_ = DebugText::GetInstance();
 	model_ = Model::Create();
 
-	worldTransform_.rotation_ = {0, 0, 0};
 
-	worldTransform_.Initialize();
+	worldTransform_[0].rotation_ = {0, 0, 0};
 
-	viewProjection_.eye = {0, 20, -30};
+	worldTransform_[1].translation_ = {-15, 0, 15};
+	worldTransform_[2].translation_ = {15, 0, 15};
+	worldTransform_[3].translation_ = {-15, 0, -15};
+	worldTransform_[4].translation_ = {15, 0, -15};
+
+	//ワールドトランスフォームの初期化
+	for (size_t i = 0; i < _countof(worldTransform_); i++) {
+		worldTransform_[i].Initialize();
+	}
+	//ビュープロジェクションの初期座標
+	viewProjection_.eye = {0, viewLengthY, viewLengthZ};
 	viewProjection_.target = {0, 0, 0};
 	viewProjection_.up = {0.0f, 1.0f, 0.0f};
 
@@ -34,42 +43,73 @@ void GameScene::Initialize() {
 
 void GameScene::Update() {
 
+	//移動用座標
 	XMFLOAT3 move = {0, 0, 0};
 
-	//回転
+	//回転速度
 	const float kRotSpeed = 0.005f;
 
-	if (input_->PushKey(DIK_U)) {
-		worldTransform_.rotation_.y += kRotSpeed;
-	} else if (input_->PushKey(DIK_I)) {
-		worldTransform_.rotation_.y -= kRotSpeed;
+	//左右回転処理
+	if (input_->PushKey(DIK_LEFT)) {
+		worldTransform_[0].rotation_.y -= kRotSpeed;
+	} else if (input_->PushKey(DIK_RIGHT)) {
+		worldTransform_[0].rotation_.y += kRotSpeed;
 	}
 
+	//移動速度
 	const float kCharSpeed = 0.05f;
 
-	//移動
+	//移動処理
 	if (input_->PushKey(DIK_UP)) {
+
 		move = {
-		  sin(worldTransform_.rotation_.y) * kCharSpeed, 0,
-		  cos(worldTransform_.rotation_.y) * kCharSpeed};
+		  sin(worldTransform_[0].rotation_.y) * kCharSpeed, 0,
+		  cos(worldTransform_[0].rotation_.y) * kCharSpeed
+		};
+
 		if (input_->PushKey(DIK_DOWN)) {
 			move = {0, 0, 0};
 		}
 	} else if (input_->PushKey(DIK_DOWN)) {
+
 		move = {
-		  -sin(worldTransform_.rotation_.y) * kCharSpeed, 0,
-		  -cos(worldTransform_.rotation_.y) * kCharSpeed};
+		  -sin(worldTransform_[0].rotation_.y) * kCharSpeed, 0,
+		  -cos(worldTransform_[0].rotation_.y) * kCharSpeed
+		};
+
 	}
+	//移動を反映
+	worldTransform_[0].translation_.x += move.x;
+	worldTransform_[0].translation_.y += move.y;
+	worldTransform_[0].translation_.z += move.z;
 
-	worldTransform_.translation_.x += move.x;
-	worldTransform_.translation_.y += move.y;
-	worldTransform_.translation_.z += move.z;
+	///---カメラ追従---///
+	//オブジェクトを追従
+	viewProjection_.target = {
+	  worldTransform_[0].translation_.x, worldTransform_[0].translation_.y,
+	  worldTransform_[0].translation_.z
+	};
 
-	worldTransform_.UpdateMatrix();
+	//カメラ位置を更新
+	viewProjection_.eye.x =
+	  worldTransform_[0].translation_.x -
+	                        sin(worldTransform_[0].rotation_.y) *
+	                          sqrt(viewLengthY * viewLengthY + viewLengthZ * viewLengthZ);
+	//viewProjection_.eye.y = move.y;
+	viewProjection_.eye.z =
+	  worldTransform_[0].translation_.z -
+	                        cos(worldTransform_[0].rotation_.y) *
+	                          sqrt(viewLengthY * viewLengthY + viewLengthZ * viewLengthZ);
+
+	//オブジェクト座標を更新
+	worldTransform_[0].UpdateMatrix();
+	//カメラ座標を更新
+	viewProjection_.UpdateMatrix();
 	
+	//デバックテキストを表示
 	debugText_->SetPos(50, 50);
 	debugText_->Printf(
-	  "target:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
+	  "eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
 
 	debugText_->SetPos(50, 70);
 	debugText_->Printf(
@@ -88,8 +128,7 @@ void GameScene::Update() {
 
 	debugText_->SetPos(50, 150);
 	debugText_->Printf(
-	  "Root:(%f,%f,%f)", worldTransform_.translation_.x,
-	  worldTransform_.translation_.y, worldTransform_.translation_.z);
+	  "translation:(%f,%f,%f)", worldTransform_[0].translation_.x, worldTransform_[0].translation_.y, worldTransform_[0].translation_.z);
 }
 
 void GameScene::Draw() {
@@ -119,7 +158,9 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 	/*model_->Draw(worldTransform_[PartId::Root], viewProjection_, textureHandle_);*/
-	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	for (size_t i = 0; i < _countof(worldTransform_); i++) {
+		model_->Draw(worldTransform_[i], viewProjection_, textureHandle_);
+	}
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
